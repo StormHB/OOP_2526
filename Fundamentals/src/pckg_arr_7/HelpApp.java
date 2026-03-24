37431f725b8c80becadd184a6904d8911083ec33
dStormHBb <martinprtenjaca@gmail.com>
2026-03-24 13:38:06 +0100

    pckg_arr_7 HelpApp created

---
+++ Fundamentals/src/pckg_arr_7/HelpApp.java
@@ -0,0 +1,44 @@
+package pckg_arr_7;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class HelpApp {
+
+    public static final int ROWS = 3;
+    public static final int COLS = 4;
+    public static final int MIN = 0;
+    public static final int MAX = 100;
+
+    public static int [][] genArray () {
+        int [][] arr = new int[ROWS][COLS];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr [i][j] = ThreadLocalRandom.current().nextInt(MIN, MAX);
+            }
+        }
+        return arr;
+    }
+
+    public static int findMax(int [][] arr) {
+        int max = arr [0][0];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr [i][j] > max) {
+                    max = arr [i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void print2DArray (int [][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr [i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Fundamentals/src/pckg_arr_7/MainApp.java
@@ -0,0 +1,5 @@
+package pckg_arr_7;
+
+public class MainApp {
+
+}
