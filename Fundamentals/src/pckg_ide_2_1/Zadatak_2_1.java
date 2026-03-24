21612e65722a9940d1ba722612a41e8d9f7bf363
dStormHBb <martinprtenjaca@gmail.com>
2026-03-24 20:57:56 +0100

    pckg_ide creation, pr_1 and zdtk_2_1 pushed

---
+++ Fundamentals/src/pckg_ide_2_1/Zadatak_2_1.java
@@ -0,0 +1,22 @@
+package pckg_ide_2_1;
+
+import java.util.Scanner;
+
+public class Zadatak_2_1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number which you want to calculate the square and cube of: ");
+        double number = sc.nextDouble();
+
+        double square = number * number;
+        double cube = number * number * number;
+
+        System.out.println("Square: " + square);
+        System.out.println("Cube: " + cube);
+
+        sc.close();
+    }
+}
